f92605dad41180595934b775527f9212cc08cc23
angi <99392768+SoyAngi@users.noreply.github.com>
2025-05-25 17:39:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/', (req, res) => {
+app.get('/designs', (req, res) => {
   res.sendFile(path.join(__dirname, 'designs.html'));
 });
 
